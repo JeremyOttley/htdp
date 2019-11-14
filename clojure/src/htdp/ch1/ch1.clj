f0e9779ffb38cf67e658bee10be572787b8b6271
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-14 09:49:33 -0500

    Update ch1.clj

--- clojure/src/htdp/ch1/ch1.clj
+++ clojure/src/htdp/ch1/ch1.clj
@@ -1,2 +1,9 @@
 (ns htdp.ch1)
 
+(defn distance-of-point 
+  "Ex. 11: Define a function that consumes two numbers, x and y, 
+   and that computes the distance of point (x,y) to the origin."
+  [x y] 
+  (Math/sqrt (+ (* x x) (* y y))))
+
+
