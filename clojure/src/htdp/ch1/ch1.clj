d501bdf3898535a4bd44ce941b250e0ebf8cf2f0
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-14 13:25:11 -0500

    Update ch1.clj

--- clojure/src/htdp/ch1/ch1.clj
+++ clojure/src/htdp/ch1/ch1.clj
@@ -6,6 +6,17 @@
   [x y] 
   (Math/sqrt (+ (* x x) (* y y))))
 
+(defn cvolume
+  "Ex. 12: Define the function cvolume, which accepts the length of a side of an equilateral cube 
+  and computes its volume."
+  [length]
+  (* length length length))
+
+(defn csurface
+  "If you have time, consider defining csurface, too."
+  [length]
+  (* (* length length) 6))
+
 (defn string-first
   "Ex. 13: Define the function string-first, 
   which extracts the first 1String from a non-empty string."
