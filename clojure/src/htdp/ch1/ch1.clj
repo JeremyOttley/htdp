a40c7bf88b89b542ac07121433a6e7f9b1bef88e
Jeremy Ottley <Gazaunga@gmail.com>
2019-11-14 13:24:04 -0500

    Update ch1.clj

--- clojure/src/htdp/ch1/ch1.clj
+++ clojure/src/htdp/ch1/ch1.clj
@@ -6,4 +6,8 @@
   [x y] 
   (Math/sqrt (+ (* x x) (* y y))))
 
-
+(defn string-first
+  "Ex. 13: Define the function string-first, 
+  which extracts the first 1String from a non-empty string."
+  [string]
+  (first (clojure.string/split string #"")))
